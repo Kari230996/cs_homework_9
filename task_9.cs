e89643a3a88e68835c8510dffdf21fb9922f3b2b
Karina <claro.claire1996@gmail.com>
2022-12-01 19:54:49 +0300

    added homework

---
+++ task_9.cs
@@ -0,0 +1,79 @@
+﻿// 1. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+void PrintNumbers(int start, int end)
+{
+    Console.Write($"{start} ");
+    if (start == end) return;
+    PrintNumbers(start - 1, end);
+}
+
+PrintNumbers(n, 1);
+
+
+// 2. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+/*
+
+Console.Write("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+void PrintNumbers(int start, int end, int sum)
+{
+    
+    
+    if (m > n) {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+
+    sum += m++;
+    PrintNumbers(m, n, sum);
+    
+    
+}
+
+PrintNumbers(m, n, 0);
+
+*/
+
+// 3. Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+/*
+Console.Write("Введите M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+
+
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n));
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+
+    }
+
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+
+AkkermanFunction(m, n);
+
+*/
